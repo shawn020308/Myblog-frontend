8ebd1389fbef9401a311bf45a1ab6dea94cbcb82
ruanshining <ruanshining@outlook.com>
2024-10-18 23:56:55 +0800

    feat: try element-plut improve the effect

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 
 <template>
-  <div id="app">
-    <router-view>
+  <router-view>
 
-    </router-view>
-  </div>
+  </router-view>
 </template>
 
 <style scoped>
-html,
-body,
-#app{
-  height: 100%;
-  width: 100%;
-  padding: 0;
-  margin: 0;
-  overflow: hidden;
-}
 </style>
+<script setup lang="ts">
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,21 +1,75 @@
 <template>
   <div class="register">
-    <section class="form_container">
-      <div class="manage_tip">
-        <div class="title">博文后台管理系统</div>
-      </div>
-    </section>
+    <h1 class="register-title">Blog Management System</h1>
+    <el-form :model="form" label-width="auto">
+      <el-form-item label="Your Name" prop="name">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="Your Email Address" prop="email">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="Your Password" prop="password">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Create Your Account</el-button>
+        <el-button>Cancel</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
+<style scoped>
+
+
+.register {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  padding: 20px;
+}
+.register-title{
+  font-size: 18px;
+  margin-bottom: 20px;
+  color: #333;
+  text-align: center;
+  font-weight: 700;
+}
+@media (min-width: 1024px) {
+  .register-title {
+    font-size: 24px;
+    margin-bottom: 30px;
+  }
+}
+</style>
+
+<script setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
+
+
 <script>
 export default {
-  name: 'Index',
+  name: 'register',
   components: {},
 }
 </script>/
 
-
-<style scoped>
-.register{position:relative;width: 100%;height: 100%;background:url("")}
-</style>
